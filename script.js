2ab9eb4ec6523baf61720d2a73edbb0d1f9dc1d2
Billskuyy.https <nabildarulfaris@gmail.com>
2026-06-07 05:23:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ initFirebaseListeners();
 // ==== KONFIGURASI GEOFENCING ====
 const KANTOR_LAT = -6.818772;
 const KANTOR_LON = 108.629543;
-const BATAS_JARAK_METER = 100;
+const BATAS_JARAK_METER = 10000;
 
 function hitungJarak(lat1, lon1, lat2, lon2) {
     const R = 6371e3;
